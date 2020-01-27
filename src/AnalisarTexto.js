5d52686043f56fab443b860758e2e419a3cc6e54
Cunhadaniel <daniel.meskita1997@gmail.com>
2020-01-26 22:02:58 -0300

    analisar texto funcional

--- src/AnalisarTexto.js
+++ src/AnalisarTexto.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function AnalisarTexto() {
-  return (
-    <div className="gameboy">
-      <div className="screen-cont">
-        <div className="screen">
-          <div className="header">Processamento de linguagem Natural</div>
-          <div className="animated-text">PLN<div className="copy"></div>
-            <form className="form-analise">
-              <textarea rows="10" placeholder="Cole aqui o texto que você deseja analisar"></textarea>
-              <br/>
-              <input type='submit'></input>
-            </form>
+
+export default class AnalisarTexto extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      analise: [],
+      flag: false
+    }
+  }
+
+  analisarTexto = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const analise = res.data;
+        this.setState({ analise, flag:true });
+      })
+  }
+
+  render() {
+    if (!this.state.flag) {
+      return (
+        <div className="gameboy">
+          <div className="screen-cont">
+            <div className="screen">
+              <div className="header">Processamento de linguagem Natural</div>
+              <div className="animated-text">PLN<div className="copy"></div>
+                <div className="form-analise">
+                  <textarea rows="10" placeholder="Cole aqui o texto que você deseja analisar"></textarea>
+                  <br />
+                  <a href="#" onClick={() => { this.analisarTexto() }}>Analisar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="controls-cont">
+            <div className="btn-direction">
+              <div className="vertical"></div>
+              <div className="horizontal"></div>
+            </div>
+            <div className="btn-AB"></div>
           </div>
         </div>
-      </div>
-      <div className="controls-cont">
-        <div className="btn-direction">
-          <div className="vertical"></div>
-          <div className="horizontal"></div>
+      );
+    }else {
+      return (
+        <div className="gameboy">
+          <div className="screen-cont">
+            <div className="screen">
+              <div className="header">Processamento de linguagem Natural</div>
+              <div className="animated-text">PLN<div className="copy"></div>
+                <div className="form-analise">
+                  {this.state.analise.map(item =>{
+                    return item.name
+                  })}
+                  <br/>
+                  <a href="/" >Home</a>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div className="controls-cont">
+            <div className="btn-direction">
+              <div className="vertical"></div>
+              <div className="horizontal"></div>
+            </div>
+            <div className="btn-AB"></div>
+          </div>
         </div>
-        <div className="btn-AB"></div>
-      </div>
-    </div>
-  );
-}
+      );
+    }
 
-export default AnalisarTexto;
+  }
+}
